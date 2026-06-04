56a1424f86afdf72e87c5e9c278ef6f853deeee2
gvega <gvega@soincon.es>
2026-06-04 14:53:37 +0200

    feat: global exceptions v1.1

--- src/main/java/com/worktrack/worktrack/shared/GlobalExceptionHandler.java
+++ src/main/java/com/worktrack/worktrack/shared/GlobalExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.worktrack.worktrack.shared;
 
 import com.worktrack.worktrack.shared.ErrorResponse;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.MessageSource;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,21 +14,18 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+import java.util.Locale;
 import java.util.Map;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Slf4j
 @RestControllerAdvice
+@RequiredArgsConstructor
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleResourceNotFound(ResourceNotFoundException ex) {
-        log.warn("Resource not found: {}", ex.getMessage());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
-                new ErrorResponse(404, ex.getMessage(), LocalDateTime.now(), null)
-        );
-    }
+    private final MessageSource messageSource;
+
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handleValidation(MethodArgumentNotValidException ex) {
@@ -59,4 +58,25 @@ public class GlobalExceptionHandler {
                 new ErrorResponse(500, "An unexpected error occurred", LocalDateTime.now(), Map.of("errorId", errorId))
         );
     }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGlobalException(Exception ex, Locale locale) {
+
+        String errorId = UUID.randomUUID().toString();
+
+        log.error("UNEXPECTED ERROR [ID: {}]: {} - Trace: ", errorId, ex.getMessage(), ex);
+
+        Map<String, String> details = Map.of("errorId", errorId);
+        return buildResponse("error.internal.server", HttpStatus.INTERNAL_SERVER_ERROR, locale, details);
+    }
+
+    private ResponseEntity<ErrorResponse> buildResponse(String messageKey, HttpStatus status, Locale locale, Map<String, String> details) {
+
+        // Fetch the localized message using the message key and locale
+        String message = messageSource.getMessage(messageKey, null, messageKey, locale);
+        // Create an ErrorResponse object with the status, message, timestamp, and details
+        ErrorResponse error = new ErrorResponse(status.value(), message, LocalDateTime.now(), details);
+        return new ResponseEntity<>(error, status);
+    }
+    
 }
